53f1e0f08855beec0068138f64274953f1877c82
cldcvr-jeewan <ldcvr-jeewan@tokopedia.com>
2021-05-04 18:29:25 +0800

    fix typo

--- hyphen.py
+++ hyphen.py
@@ -23,7 +23,7 @@ def get_hit_count():
 def hit():
 #    count = get_hit_count()
 #    return 'This is the %i accessed this site since startup.\n' % int(count)
-    return 'This is the accessed this site since startup.\n' % int(count)
+    return 'This is the accessed this site since startup.\n' 
 
 if __name__ == "__main__":
     web.run(host="0.0.0.0", debug=True)
