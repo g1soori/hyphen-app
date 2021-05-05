f1390b033a0d5874f5f741288fa45010a41d4ae5
g1soori <ldcvr-jeewan@tokopedia.com>
2021-05-05 14:37:23 +0800

    cleanup code

--- hyphen.py
+++ hyphen.py
@@ -1,29 +1,41 @@
 
-import time
-import redis
-from flask import Flask
+from flask import Flask, request
+import json
 
-web = Flask(__name__)
-cache = redis.Redis(host='redis-lb', port=6379)
+class AppService:
+    
+    tasks = [
+        {
+            'id': 1,
+            'name': "Jeewan",
+            "role": "DevOps"
+        },
+        {
+            "id": 2,
+            "name": "John",
+            "role": "Engineer"
+        }
+    ]
+
+    def __init__(self):
+        self.tasksJSON = json.dumps(self.tasks)
+
+    def get_tasks(self):
+        return self.tasksJSON
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+app = Flask(__name__)
+appService = AppService()
 
 
-@web.route('/')
-def hit():
-#    count = get_hit_count()
-#    return 'This is the %i accessed this site since startup.\n' % int(count)
-    return 'This is the accessed this site since startup.\n' 
+@app.route('/')
+def home():
+    return "App Works!!!"
+
+
+@app.route('/api/tasks')
+def tasks():
+    return appService.get_tasks()
 
 if __name__ == "__main__":
-    web.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)
